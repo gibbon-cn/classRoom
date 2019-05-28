9041d135281ac25c5c715590d11be2dd6c2d1d55
Weipeng Ji(纪鹏) <jip@inspur.com>
2019-05-28 19:15:24 +0800

    学习npm包

---
+++ firstNpmPackage/README.md
@@ -0,0 +1,3 @@
+# firstNpmPackage
+
+学习制作基于ts的npm包，参照：https://www.jianshu.com/p/8fa2c50720e4
---
+++ firstNpmPackage/lib/index.ts
@@ -0,0 +1,7 @@
+export function hello() {
+    console.log('hello world!');
+}
+
+export function add(a:number, b:number) : number {
+    return a + b;
+  }
